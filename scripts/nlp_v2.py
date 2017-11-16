66e38837fcaa798588e5b486ab09ece436d59e2b
Charles Winston <charles.winston@tufts.edu>
2017-11-16 14:35:01 -0500

    WE GOT GOOGLE

--- scripts/nlp_v2.py
+++ scripts/nlp_v2.py
@@ -1,25 +1,54 @@
 # -*- coding: utf-8 -*-
 # neural_news
-from textblob import TextBlob
+#from textblob import TextBlob
 import json
 import sys
 
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
 def analysis(text):
-    blob      = TextBlob(text)
-    sentences = blob.sentences
-    result    = []
-    for sentence in sentences:
-        try:
-            result.append({
-                "text"     : str(sentence),
-                "polarity" : str(sentence.polarity)
+    # Instantiates a client
+    client = language.LanguageServiceClient()
+
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    analysis_result = client.analyze_entity_sentiment(document)
+    final_result = []
+
+    for entity in analysis_result.entities:
+        #print('Mentions: ')
+        #print(u'Name: "{}"'.format(entity.name))
+        current_ent = {
+            'name'      :   entity.name,
+            'mentions'  :   [],
+        }
+        for mention in entity.mentions:
+            #print(u'  Begin Offset : {}'.format(mention.text.begin_offset))
+            #print(u'  Content : {}'.format(mention.text.content))
+            #print(u'  Magnitude : {}'.format(mention.sentiment.magnitude))
+            #print(u'  Sentiment : {}'.format(mention.sentiment.score))
+            #print(u'  Type : {}'.format(mention.type))
+            current_ent['mentions'].append({
+                'begin_offset'  :   mention.text.begin_offset,
+                'content'       :   mention.text.content,
+                'magnitude'     :   mention.sentiment.magnitude,
+                'sentiment'     :   mention.sentiment.score,
+                'type'          :   mention.type,
             })
-        except:
-            result.append({
-                "text" : "nothing",
-                "polarity" : "0"
-            })
-    print(json.dumps(result))
+        #print(u'Salience: {}'.format(entity.salience))
+        current_ent['salience'] = entity.salience
+        #print(u'Magnitude: {}\n'.format(entity.sentiment.magnitude))
+        current_ent['magnitude'] = entity.sentiment.magnitude
+        #print(u'Sentiment: {}\n'.format(entity.sentiment.score))
+        current_ent['sentiment'] = entity.sentiment.score
+        final_result.append(current_ent)
+
+    print(json.dumps(final_result))
     sys.stdout.flush()
 
 def main():
